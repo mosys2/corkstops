303f22643918d7a12aded0d8d133cada584ac84e
Mohammad saadati <mohammadsaadati117@gmail.com>
2023-05-17 11:56:59 +0430

    first commit

---
+++ Store.Common/Constant/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Common.Constant
+{
+    public class Messages
+    {
+        public class ErrorsMessage
+        {
+            public const string Register = "Succesfull";
+        }
+    }
+}
